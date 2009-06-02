57b4841064f41b50a739161def31841ac24ec09b
Keith Rarick <kr@xph.us>
2009-06-02 16:36:24 +0200

    Link arbitrary files.

--- http.c
+++ http.c
@@ -9,8 +9,8 @@
 #include "http.h"
 #include "util.h"
 
-#define ROOT_RESPONSE ("This is " PACKAGE_STRING ".")
-#define ROOT_RESPONSE_LEN (sizeof(ROOT_RESPONSE) - 1)
+extern const char root_html[];
+extern const int root_html_size;
 
 void
 http_handle_generic(struct evhttp_request *req, void *data)
@@ -24,6 +24,6 @@ http_handle_root(struct evhttp_request *req, void *data)
     struct evbuffer *out;
 
     out = evbuffer_new();
-    evbuffer_add_reference(out, ROOT_RESPONSE, ROOT_RESPONSE_LEN, 0, 0);
+    evbuffer_add_reference(out, root_html, root_html_size, 0, 0);
     evhttp_send_reply(req, HTTP_OK, "OK", out);
 }
