5af084db4b7611ce1c92f07f9a6791429ef1045c
Keith Rarick <kr@xph.us>
2010-02-21 23:46:03 -0800

    Test key comparison a bit.

--- check-key.c
+++ check-key.c
@@ -5,6 +5,9 @@
 #include "key.h"
 #include "sha512.h"
 
+static uint32_t x[3] = { 0x6b86b273LU, 0xff34fce1LU, 0x9d6b804eLU },
+                y[3] = { 0xd4735e3aLU, 0x265e16eeLU, 0xe03f5971LU };
+
 void
 __CUT_BRINGUP__void_key()
 {
@@ -27,6 +30,34 @@ __CUT__void_key_test_key_fmt()
     sha512(input, strlen(input), key, 12);
     key_fmt(got, key);
     ASSERT(!strcmp(exp, got), "got %s", got);
+}
+
+void
+__CUT__void_key_test_key_cmp_a()
+{
+    uint32_t c[3];
+
+    c[0] = x[0];
+    c[1] = x[1];
+    c[2] = x[2];
+    c[2]++;
+
+    int r = key_distance_cmp(c, x, y);
+    ASSERT(r == -1, "x should be closer");
+}
+
+void
+__CUT__void_key_test_key_cmp_b()
+{
+    uint32_t c[3];
+
+    c[0] = y[0];
+    c[1] = y[1];
+    c[2] = y[2];
+    c[2]++;
+
+    int r = key_distance_cmp(c, x, y);
+    ASSERT(r == 1, "y should be closer");
 }
 
 void
